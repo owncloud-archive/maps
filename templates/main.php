f164c5c2b1d830d0de27a01216bf4729079aaf0b
Vinzenz Rosenkranz <vinzenz.rosenkranz@gmail.com>
2015-08-13 23:04:36 +0200

    split gpx section in different actions

--- templates/main.php
+++ templates/main.php
@@ -47,7 +47,12 @@
 			</li>
 			<?php endif; ?>
 			<li>
-				<a class='trackLayer icon-contacts-dark' data-layer="tracks">Show GPX-Tracks</a>
+				<a class='trackLayer icon-link toggle-children' data-layer="tracks">Show GPX-Tracks</a>
+				<ul id="track-options" class="hidden">
+					<li><a class="trackOptions" data-layerGroup="tracks" data-layerValue="load">Load from MyTracks folder</a></li>
+					<li><a class="trackOptions" data-layerGroup="tracks" data-layerValue="choose">Choose from files App</a></li>
+					<li><a class="trackOptions" data-layerGroup="tracks" data-layerValue="upload">Upload GPX File</a></li>
+				</ul>
 			</li>
 			<li>
 				<a class='main-cat-layer icon-info toggle-children' data-layer="amenity">Amenity</a>
