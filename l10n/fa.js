f52e75d693ffdb77f08bd8c80e1de1b4aa58dc48
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-19 00:50:33 +0000

    [tx-robot] updated from transifex

--- l10n/fa.js
+++ l10n/fa.js
@@ -5,4 +5,4 @@ OC.L10N.register(
     "left" : "چپ",
     "right" : "راست"
 },
-"nplurals=1; plural=0;");
+"nplurals=2; plural=(n > 1);");
--- l10n/kn.js
+++ l10n/kn.js
@@ -5,4 +5,4 @@ OC.L10N.register(
     "left" : "ಎಡ",
     "right" : "ಬಲ"
 },
-"nplurals=1; plural=0;");
+"nplurals=2; plural=(n > 1);");
